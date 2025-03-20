948eb7b03ed7182fb74f8eac7a2aa1b7d04e6180
Jan Henk Hazelaar <janhenk@concept7.nl>
2025-03-20 12:24:54 +0100

    Add property changes events

---
+++ src/Events/ContactPropertyChange.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Concept7\HubspotWebhookClient\Events;
+
+class ContactPropertyChange extends Event {}
--- src/Jobs/ProcessWebhookJob.php
+++ src/Jobs/ProcessWebhookJob.php
@@ -14,10 +14,13 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
             match ($event['subscriptionType']) {
                 'company.creation' => Events\CompanyCreation::dispatch($event),
                 'company.deletion' => Events\CompanyDeletion::dispatch($event),
+                'company.propertyChange' => Events\CompanyPropertyChange::dispatch($event),
                 'contact.creation' => Events\ContactCreation::dispatch($event),
                 'contact.deletion' => Events\ContactDeletion::dispatch($event),
+                'contact.propertyChange' => Events\ContactPropertyChange::dispatch($event),
                 'deal.creation' => Events\DealCreation::dispatch($event),
                 'deal.deletion' => Events\DealDeletion::dispatch($event),
+                'deal.propertyChange' => Events\DealPropertyChange::dispatch($event),
                 'line_item.propertyChange' => Events\LineItemPropertyChange::dispatch($event),
                 default => throw new UnhandledMatchError,
             };
